fff2019a99040a97176b8e5bdf5b1862b44aa3e0
Ivan Perez <ivan_psh@hotmail.com>
2023-03-22 21:20:27 +0100

    Update

---
+++ app/src/main/java/es/ieslavereda/myrecicleviewexample/MyRecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package es.ieslavereda.myrecicleviewexample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    private UsuarioRepository repository;
+    private final LayoutInflater inflater;
+
+    public MyRecyclerViewAdapter(Context context) {
+        repository = UsuarioRepository.getInstance();
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.simple_element, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Usuario u = repository.getUsuario(position);
+
+        holder.nombre.setText(u.getApellidos()+", " +u.getNombre());
+        holder.oficio.setText(u.getOficio());
+        holder.imagen.setImageResource(u.getImagen());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return repository.getSize();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView nombre;
+        private TextView oficio;
+        private ImageView imagen;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nombre = itemView.findViewById(R.id.textViewNombre);
+            oficio = itemView.findViewById(R.id.textViewOficio);
+            imagen = itemView.findViewById(R.id.imageView);
+
+        }
+
+    }
+}
--- app/src/main/java/es/ieslavereda/myrecicleviewexample/UsuarioRepository.java
+++ app/src/main/java/es/ieslavereda/myrecicleviewexample/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package es.ieslavereda.myrecicleviewexample;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioRepository {
@@ -8,9 +9,19 @@ public class UsuarioRepository {
     private static UsuarioRepository instance;
 
     private UsuarioRepository(){
-
-
-
+        usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Manolo", "García Pérez", "Actor", R.mipmap.ic_actor_foreground));
+        usuarios.add(new Usuario("Luis", "García Pérez", "Albanyil", R.mipmap.ic_albanyil_foreground));
+        usuarios.add(new Usuario("Carlos", "García Pérez", "Banquero", R.mipmap.ic_banquero_foreground));
+        usuarios.add(new Usuario("Miguel", "García Pérez", "Cocinero", R.mipmap.ic_cocinero_foreground));
+        usuarios.add(new Usuario("Pepe", "García Pérez", "Actor", R.mipmap.ic_estudiante_foreground));
+        usuarios.add(new Usuario("Alfonso", "García Pérez", "Actor", R.mipmap.ic_instagramer_foreground));
+        usuarios.add(new Usuario("Rubén", "García Pérez", "Actor", R.mipmap.ic_pintor_foreground));
+        usuarios.add(new Usuario("Alejandro", "García Pérez", "Actor", R.mipmap.ic_policia_foreground));
+        usuarios.add(new Usuario("Ivan", "García Pérez", "Actor", R.mipmap.ic_politico_activo_foreground));
+        usuarios.add(new Usuario("Ramon", "García Pérez", "Actor", R.mipmap.ic_politico_retirado_foreground));
+        usuarios.add(new Usuario("Raul", "García Pérez", "Actor", R.mipmap.ic_vendedor_foreground));
+        usuarios.add(new Usuario("Javi", "García Pérez", "Actor", R.mipmap.ic_youtuber_foreground));
     }
 
     public static UsuarioRepository getInstance(){
@@ -18,5 +29,13 @@ public class UsuarioRepository {
             instance = new UsuarioRepository();
 
         return instance;
+    }
+
+    public Usuario getUsuario(int index) {
+        return usuarios.get(index);
+    }
+
+    public int getSize(){
+        return usuarios.size();
     }
 }

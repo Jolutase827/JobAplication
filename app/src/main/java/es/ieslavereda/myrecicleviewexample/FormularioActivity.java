2285e699d74cc24a79e96f67d3b710622509216f
jtarragas <cau01111@gmail.com>
2023-03-28 21:08:25 +0200

    28/03/2023

--- app/src/main/java/es/ieslavereda/myrecicleviewexample/FormularioActivity.java
+++ app/src/main/java/es/ieslavereda/myrecicleviewexample/FormularioActivity.java
@@ -2,13 +2,41 @@ package es.ieslavereda.myrecicleviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class FormularioActivity extends AppCompatActivity {
-
+    private Button aceptar;
+    private Button cancelar;
+    private Spinner spinner;
+    private EditText nombre;
+    private EditText apellido;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        aceptar = findViewById(R.id.buttonAceptar);
+        cancelar = findViewById(R.id.botonCancelar);
+        nombre = findViewById(R.id.nombre);
+        apellido = findViewById(R.id.apellido);
+        aceptar.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.putExtra("Nombre",nombre.getText().toString());
+            intent.putExtra("Apellido", apellido.getText().toString());
+            intent.putExtra("Oficio",spinner.getBaseline());
+            setResult(RESULT_OK,intent);
+            finish();
+        });
+        cancelar.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            setResult(RESULT_CANCELED,intent);
+            finish();
+        });
+
     }
 }
--- app/src/main/java/es/ieslavereda/myrecicleviewexample/MainActivity.java
+++ app/src/main/java/es/ieslavereda/myrecicleviewexample/MainActivity.java
@@ -1,13 +1,18 @@
 package es.ieslavereda.myrecicleviewexample;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -27,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == RESULT_CANCELED)
+                        Toast.makeText(this, "Cancelado por el usuario", Toast.LENGTH_LONG).show();
+                    else if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Usuario usuario = new Usuario(data.getExtras().getString("Nombre"),data.getExtras().getString("Apellido"),data.getExtras().getString("Nombre"),R.mipmap.ic_actor);
+                        UsuarioRepository.getInstance().add(UsuarioRepository.getInstance().getSize(),);
+                        Toast.makeText(this, "Nuevo, aceptado por " + name , Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
 
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT) {
             @Override
